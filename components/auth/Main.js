772e09646c0eee7d68d4be07115827df789ec550
Muhammad Shahbaz <M023Shah@gmail.com>
2022-06-18 17:48:27 -0700

    adding redux

---
+++ components/auth/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+    componentDidMount(){
+        
+    }
+
+    render() {
+    return (
+    
+    <div>
+        Main
+    </div>
+    )
+
+}
+}
+
+export default Main
---
+++ redux/reducers/user.js
@@ -0,0 +1,10 @@
+const initialState = {
+    currentUser: null
+}
+
+export const user = (state = initialState, actions) => {
+    return {
+        ...state,
+        currentUser: actions.currentUser
+    }
+}
